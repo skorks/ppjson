9b9c4675449985546ab4aadf5b09ba5e8d6165ef
Alan Skorkin <alan@skorks.com>
2013-02-28 16:45:17 +1100

    Implements some more options

--- lib/ppjson/commands/pretty_print_json.rb
+++ lib/ppjson/commands/pretty_print_json.rb
@@ -2,13 +2,47 @@ module Ppson
   module Commands
     class PrettyPrintJson < ::Escort::ActionCommand::Base
       def execute
-        arguments.each do |argument|
-          hash = from_json(argument.to_s)
-          $stdout.puts MultiJson.dump(hash, :pretty => true)
+        if command_options[:file] && command_options[:inline] && command_options[:undo]
+        elsif command_options[:file] && command_options[:inline]
+          pretty_print_json_string_back_to_file
+        elsif command_options[:file] && command_options[:undo]
+        elsif command_options[:file]
+          pretty_print_json_strings_to_stream(json_strings_from_file_arguments)
+        else
+          pretty_print_arguments_to_stdout
         end
       end
 
       private
+
+      def json_strings_from_file_arguments
+        json_strings = []
+        arguments.each do |filepath|
+          begin
+            File.open(filepath, 'r') do |file|
+              json_strings << file.readlines.join("")
+            end
+          rescue => e
+            Escort::Logger.error.warn("Error reading file #{filepath}")
+          end
+        end
+        json_strings
+      end
+
+      def pretty_print_arguments_to_stdout
+        pretty_print_json_strings_to_stream(arguments.map(&:to_s), $stdout)
+      end
+
+      def pretty_print_json_strings_to_stream(json_strings, stream = $stdout)
+        json_strings.each do |json_string|
+          pretty_print_to_stream(json_string, stream)
+        end
+      end
+
+      def pretty_print_to_stream(json_string, stream)
+        hash = from_json(json_string)
+        stream.puts MultiJson.dump(hash, :pretty => true)
+      end
 
       def from_json(json)
         MultiJson.load(json)
